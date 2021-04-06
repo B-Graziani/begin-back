d38121495f0afaedd7231d37657918ee6ce5a98a
benoit graziani <benoit.c.graziani@hotmail.fr>
2021-04-06 17:06:59 +0200

    exo

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,15 @@
 class PagesController < ApplicationController
   def home
   end
+  def about
+  end
+
+  def contact
+    @members = ['thanh', 'germaiins', "dimitri", "boris", "damien"]
+
+    if params[:member]
+      @members = @members.select{ |member| member.start_with?(params[:member]) }
+    end
+  end
+
 end
